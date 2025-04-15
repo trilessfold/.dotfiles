9127fd2d0c6acb752a7dc81536d684c94b755dde
Artyom Shcherba <artem.shcherba@eltex.loc>
2025-04-15 12:56:11 +0700

    tmux: vim, navigation; fugitive status

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,4 @@ vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", {desc = "Open Diffvie
 vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<cr>", {desc = "Buffer blame (fugitive)"})
 vim.keymap.set("n", "<leader>gB", "<cmd>.DiffviewFileHistory <cr>", {desc = "Line history"})
 vim.keymap.set("n", "<leader>gF", "<cmd>DiffviewFileHistory %<cr>", {desc = "File history"})
+vim.keymap.set("n", "<leader>gs", "<cmd>Git<cr>", {desc = "Git status (fugitive)"})
