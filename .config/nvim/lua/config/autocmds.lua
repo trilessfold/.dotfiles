1deb203d48f3db8ba4017774330fb39f7040864c
Artyom Shcherba <artem.shcherba@eltex.loc>
2025-04-16 16:45:10 +0700

    nvim: added folds for commit view in fugitive

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,10 +7,14 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- Disable autoformat for lua files
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "*" },
-  callback = function()
-    vim.b.autoformat = false
-  end,
+    pattern = { "*" },
+    callback = function()
+        vim.b.autoformat = false
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = { "git" },
+    command = "set foldmethod=syntax",
 })
