289ba6f5038611cc7fbda4021f656ce5507ccb94
Artyom Shcherba <artem.shcherba@eltex.loc>
2025-07-03 00:24:59 +0700

    nvim: removed useless autocmd for fugitive C-O

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,14 +8,3 @@ vim.keymap.set("n", "<leader>gB", "<cmd>.DiffviewFileHistory <cr>", { desc = "Li
 vim.keymap.set("n", "<leader>gF", "<cmd>DiffviewFileHistory %<cr>", { desc = "File history" })
 vim.keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "Git status (fugitive)" })
 
-vim.keymap.set("n", "<C-O>", function()
-    local bufname = vim.api.nvim_buf_get_name(0)
-    local is_fugitive = bufname:match("^fugitive://")
-    local is_diff = vim.wo.diff
-
-    if is_fugitive or is_diff then
-        -- Останавливаем вставку, если она есть
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("dq", true, false, true), "m", false)
-    end
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-O>", true, false, true), "n", false)
-end, { noremap = true, silent = true, desc = "Smart Ctrl-O fugitive diff exit" })
