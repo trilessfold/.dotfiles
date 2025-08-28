b8d9a8498f5fde6c5e9e39b52bbc8f478c637068
Artyom Shcherba <artem.shcherba@eltex.loc>
2025-08-28 18:07:42 +0700

    nvim: commented russian map

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,9 +3,9 @@
 -- Add any additional options here
 vim.cmd("set background=dark")
 vim.cmd("set t_Co=256")
-vim.opt.langmap= {
-  "ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕHГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ;`qwertyuiop[]asdfghjkl\\;'zxcvbnm\\,.~QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>"
-}
+-- vim.opt.langmap= {
+--   "ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕHГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ;`qwertyuiop[]asdfghjkl\\;'zxcvbnm\\,.~QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>"
+-- }
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
