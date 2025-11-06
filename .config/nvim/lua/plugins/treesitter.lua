8d8027cae358d1b4add8b998e2bcdb10e7f23f5b
Artyom Shcherba <artem.shcherba@eltex.loc>
2025-11-06 18:10:14 +0700

    nvim: removed erlangls

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,18 @@
 return {
---    {
---        "nvim-treesitter/playground",
---    },
+    --    {
+    --        "nvim-treesitter/playground",
+    --    },
     {
         "nvim-treesitter/nvim-treesitter",
         opts = {
+            ensure_loaded = {
+                "erlang",
+            },
             textobjects = {
                 move = {
-                    enable = false
-                }
-            }
-        }
-    }
+                    enable = false,
+                },
+            },
+        },
+    },
 }
