795853b0dff90612cdf50a05da59098e1b2533df
Artyom Shcherba <artem.shcherba@eltex.loc>
2025-11-24 18:00:13 +0700

    nvim: removed vgig; fixed elp symlink opening

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -20,3 +20,29 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.foldmethod = "syntax"
     end,
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+    group = vim.api.nvim_create_augroup("ResolveSymlinks", { clear = true }),
+    callback = function()
+        local buf = 0
+        local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
+
+        -- Пропускаем небуквальные буферы (help, quickfix, nofile, etc.)
+        if buftype ~= "" then
+            return
+        end
+
+        local current = vim.api.nvim_buf_get_name(buf)
+        if current == "" then
+            return
+        end
+
+        -- Проверяем, что файл существует на диске
+        if vim.fn.filereadable(current) ~= 1 then
+            return
+        end
+
+        local resolved = vim.fn.resolve(current)
+        vim.cmd("file " .. resolved)
+    end,
+})
