a42cda081ac11e0ada3e610b1976e0ca3f2c1ca6
Artyom Shcherba <artem.shcherba@eltex.loc>
2025-12-24 15:18:41 +0700

    nvim: removed depracated functions in lsp config

--- .config/nvim/lua/plugins/lspserver.lua
+++ .config/nvim/lua/plugins/lspserver.lua
@@ -1,11 +1,6 @@
 return {
     {
         "neovim/nvim-lspconfig",
-        init = function()
-            local keys = require("lazyvim.plugins.lsp.keymaps").get()
-            keys[#keys + 1] = { "[[", false } -- Unbind specific key
-            keys[#keys + 1] = { "]]", false }
-        end,
         opts = {
             servers = {
                 elp = {
