6987d7a4357ad8948ff7b65939be9b1d6d50cdf4
Artyom Shcherba <artem.shcherba@eltex.loc>
2026-01-30 12:09:29 +0700

    nvim: added the ru layout

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,7 +3,9 @@
 -- Add any additional options here
 vim.cmd("set background=dark")
 vim.cmd("set t_Co=256")
+vim.cmd("set langmap=ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz")
 
+vim.opt.spelllang = 'en,ru'
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
