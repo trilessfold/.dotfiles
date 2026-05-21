b1ddbe4743ff172141133e016d7f99c77542c126
Artyom Shcherba <artem.shcherba@eltex.loc>
2026-05-21 16:15:45 +0700

    nvim: added Guit hotkeys

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,11 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", { desc = "Open Diffview" })
-vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<cr>", { desc = "Buffer blame (fugitive)" })
+vim.keymap.set("n", "<leader>gb", "<cmd>Guit blame<cr>", { desc = "Buffer blame" })
 vim.keymap.set("n", "<leader>gB", "<cmd>.DiffviewFileHistory <cr>", { desc = "Line history" })
-vim.keymap.set("n", "<leader>gF", "<cmd>DiffviewFileHistory %<cr>", { desc = "File history" })
-vim.keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "Git status (fugitive)" })
+vim.keymap.set("n", "<leader>gF", "<cmd>Guit history %<cr>", { desc = "File history" })
+vim.keymap.set("n", "<leader>gl", "<cmd>Guit log %<cr>", { desc = "File history" })
+vim.keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "Git status" })
 vim.keymap.set("n", "<leader>gc", "<cmd>0,3Git blame<cr>", { desc = "Open commit of line (fugitive)" })
 vim.keymap.set("n", "<A-h>", "<cmd>normal! H<cr>")
 vim.keymap.set("n", "<A-l>", "<cmd>normal! L<cr>")
