2ac5c452c7861e580d0a443b5920635d45d38943
Artyom Shcherba <artem.shcherba@eltex.loc>
2026-05-22 17:31:53 +0700

    nvim: fixed erlang foldings

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -25,7 +25,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "erlang",
   callback = function()
     vim.opt_local.foldmethod = "expr"
-    vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
+    vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     vim.opt_local.foldlevel = 99
   end,
 })
