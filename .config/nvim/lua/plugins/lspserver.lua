e2754cb24b75e63675aa67f9179dfb1c3e38280e
Artyom Shcherba <artem.shcherba@eltex.loc>
2026-07-16 23:09:20 +0700

    nvim: json parser

--- .config/nvim/lua/plugins/lspserver.lua
+++ .config/nvim/lua/plugins/lspserver.lua
@@ -8,14 +8,12 @@ return {
                         return require("lspconfig.util").root_pattern("cwd")(...)
                     end,
                 },
-                elixirls = {
-                    root_dir = function(...)
-                        return require("lspconfig.util").root_pattern("mix.exs")(...)
-                    end,
-                },
             },
-            inline_hints = {enabled = true},
+            inline_hints = { enabled = true },
             autoformat = false,
         },
+    },
+    {
+        "VPavliashvili/json-nvim",
     },
 }
